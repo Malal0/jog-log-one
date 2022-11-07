d8c13ff65bb60554233a62cc5b27731e61476208
UncomfortableChai <mj1055119@gmail.com>
2022-11-07 04:05:10 -0800

    update

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,41 @@
-// import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 export default function Timer() {
-    // const [min, setMin] = useState(0);
-    // const [sec, setSec] = useState(0);
+    const [min, setMin] = useState(0);
+    const [sec, setSec] = useState(0);
+    const [timerOn, setTimerOn] = useState(false);
 
+    useEffect(() => {
+        if (timerOn) {
+            const timer = setInterval(() => {
+                setSec(sec + 1);
+                if (sec === 59) {
+                    setMin(min + 1);
+                    setSec(0);
+                }
+            }, 1000)
 
+            return () => clearInterval(timer)
+        }
+    });
 
     return (
         <>
             <h2>minutes</h2>
             <h2>seconds</h2>
-            {/* <h1>
+            <h1>
                 {min < 10 ? '0' + min : min}
                 :
                 {sec < 10 ? '0' + sec : sec}
-            </h1> */}
-            <button>| |</button>
+            </h1>
+            <button
+                onClick={() => setTimerOn(!timerOn)}
+            // onMouseUp={() => {
+            //     setTimerOn(!timerOn)
+            //     setMin(0)
+            //     setSec(0)
+            // }}
+            >| |</button>
             <p>press here</p>
         </>
     )
