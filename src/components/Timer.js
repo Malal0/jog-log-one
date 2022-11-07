8a98780563f5ddc00477ab68288d68f53bcd2edc
UncomfortableChai <mj1055119@gmail.com>
2022-11-06 17:11:30 -0800

    update

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,21 @@
-import React, { useState, useEffect } from "react"
+// import React, { useState } from "react"
 
 export default function Timer() {
-    const [min, setMin] = useState(0);
-    const [sec, setSec] = useState(0);
+    // const [min, setMin] = useState(0);
+    // const [sec, setSec] = useState(0);
 
-    const startTimer = useEffect(() => {
-        const timer = setInterval(() => {
-            setSec(sec++);
-            if (sec > 59) {
-                setSec(0)
-            }
-        }, 1000)
-    })
+
 
     return (
         <>
             <h2>minutes</h2>
             <h2>seconds</h2>
-            <h1>
+            {/* <h1>
                 {min < 10 ? '0' + min : min}
                 :
                 {sec < 10 ? '0' + sec : sec}
-            </h1>
-            <button onClick={startTimer}>| |</button>
+            </h1> */}
+            <button>| |</button>
             <p>press here</p>
         </>
     )
